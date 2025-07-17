037399c6543e99a5c48905df2220dd7187073766
davidg03666 <davidg03666@gmail.com>
2025-07-17 12:51:40 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,18 +3,7 @@ function initFlipbook() {
     width: $('#flipbook').width(),
     height: $('#flipbook').height(),
     autoCenter: true,
-    when: {
-      turned: function(event, page) {
-        const audioSrc = $('#flipbook .page').eq(page-1).data('audio');
-        const narrador = document.getElementById('narrador');
-        /*if (audioSrc) {
-          narrador.src = audioSrc;
-          narrador.play();
-        } else {
-          narrador.pause();
-        }*/
-      }
-    }
+    
   });
 }
 
